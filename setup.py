928852c53e78e3c7743b242ea416269c92e29fb4
Hemanta Pokharel <hemantapkh@gmail.com>
2020-11-15 14:11:59 +0545

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="ncellapp", 
+    version="0.1.0",
+    author="Hemanta Pokharel",
+    author_email="hemantapkh@yahoo.com",
+    maintainer="Hemanta Pokharel",
+    maintainer_email="hemantapkh@yahoo.com",
+    description="Ncell unofficial API wrapper for python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["crypto"],
+    url="https://github.com/hemantapkh/ncellapp",
+    project_urls={
+        "Issue tracker": "https://github.com/hemantapkh/ncellapp/issues",
+      },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
