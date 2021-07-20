d27c880d0e1bd4e6b9fdd4912584fe1ce4f4e750
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-20 18:48:20 +0545

    Update models.py

--- ncellapp/models.py
+++ ncellapp/models.py
@@ -9,41 +9,71 @@ class NcellResponse(object):
     
     @property
     def responseHeader(self):
+        """
+        Returns a dictionary of response header from ncell
+        """
         return self.__responseDict['responseHeader']
 
     @property
     def content(self):
+        """
+        Returns the content of the response
+        """
         self.__responseDict2.pop('responseHeader', None)
         return self.__responseDict2
     
     @property
     def cookies(self):
+        """
+        Returns a CookieJar object with the cookies sent back from the server
+        """
         return self.__response.cookies
     
     @property
     def elapsed(self):
+        """
+        Returns a timedelta object with the time elapsed from sending the request to the arrival of the response
+        """
         return self.__response.elapsed
     
     @property
     def headers(self):
+        """
+        Returns a dictionary of response headers
+        """
         return self.__response.headers
     
     @property
     def ok(self):
+        """
+        Returns True if status_code is less than 400, otherwise False
+        """
         return self.__response.ok
     
     @property
     def reason(self):
+        """
+        Returns a text corresponding to the status code of the response
+        """
         return self.__response.reason
     
     @property
     def request(self):
+        """
+        Returns the request object that requested this response
+        """
         return self.__response.request
     
     @property
     def statusCode(self):
+        """
+        Returns a number that indicates the status (200 is OK, 404 is Not Found)
+        """
         return self.__response.status_code
     
     @property
     def url(self):
+        """
+        Returns the URL of the response
+        """
         return self.__response.url
